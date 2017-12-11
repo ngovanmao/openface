173139dd0fd17fb059f1d5c2d727ef5b5d976b8d
ngovanmao <newngovanmao@gmail.com>
2017-12-11 14:31:45 +0800

    Add tcpserver/tcpclient for query the image.

---
+++ demos/tcpClient.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import socket
+import argparse
+from struct import pack 
+
+HOST, PORT = "localhost", 9999
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--ipaddr',
+        type=str,
+        help='IP address of a requesting server', 
+        default='localhost')
+    parser.add_argument(
+        '--port',
+        type=int,
+        help='Listening port in the requesting server',
+        default=9999)
+    parser.add_argument(
+        '--imgdir',
+        type=str,
+        help='Image location/directory',
+        default='Rocky.jpg')
+
+    args = parser.parse_args()
+
+    # Create a socket (SOCK_STREAM means a TCP socket)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    f = open(args.imgdir, 'rb')
+    data = f.read()
+    f.close()
+
+    # Thanks for the stackoverflow answer:
+    # https://stackoverflow.com/questions/42459499/what-is-the-proper-way-of-sending-a-large-amount-of-data-over-sockets-in-python
+
+    try:
+        # Connect to server and send data
+        sock.connect((args.ipaddr, args.port))
+        # Use struct to make sure we have a consistent endiannes on the length
+        length = pack('>Q', len(data))
+        # sendall to make sure it blocks if there's back-pressure on the socket
+        sock.sendall(length)
+        sock.sendall(data)
+    
+        print "Sent:     {}".format(len(data))
+        # Receive data from the server and shut down
+        data = []
+        received = sock.recv(1024)
+        print("Received: {}".format(received))
+    finally:
+        sock.close()
+
+if __name__ == '__main__':
+    main()
